687e7353fa574d3acabee8e3885ff3ab9613e71b
wellington <wrogeriowrs@gmail.com>
2023-05-02 19:46:36 -0300

    controllers

---
+++ src/controllers/VerbsController.js
@@ -0,0 +1,78 @@
+import pool from "../database/db";
+
+export const GetAll = async () => {
+  const query = ` SELECT  *
+                  FROM    Verbs`;
+  try {
+    await pool.connect();
+    const result = await pool.request().query(query);
+    return result.recordset.length > 0 ? result.recordset : [];
+  } catch (error) {
+    return { error: error.message };
+  }
+};
+
+export const GetItem = async (id) => {
+  const query = ` SELECT  Id, FundoId, Sigla, DtLancamento, DtLancamentoDt, Ano, Mes, Dia, MesNome, Tipo, Qtd, Valor, Total 
+                  FROM    vCarteiras
+                  WHERE   Id = '${id}'`;
+  try {
+    await pool.connect();
+    const result = await pool.request().query(query);
+    return result.recordset.length > 0 ? result.recordset[0] : {};
+  } catch (error) {
+    return { error: error.message };
+  }
+};
+
+export const ConfereQtdSigla = async (ano, mes, sigla) => {
+  const query = ` SELECT Sigla, Qtd, Valor, Total FROM fnCarteirasConferencia(${ano},${mes}, '${sigla}')`;
+  try {
+    await pool.connect();
+    const result = await pool.request().query(query);
+    return result.recordset.length > 0 ? result.recordset[0] : {};
+  } catch (error) {
+    return { error: error.message };
+  }
+};
+
+export const SaveItem = async (item) => {
+  const query = ` INSERT INTO Carteiras (FundoId, DtLancamento, Tipo, Qtd, Valor) 
+                  VALUES ('${item.FundoId}', '${item.DtLancamento}', '${item.Tipo}', '${item.Qtd}', '${item.Valor}')`;
+  try {
+    await pool.connect();
+    await pool.request().query(query);
+    return true;
+  } catch (error) {
+    return false
+  }
+}
+
+export const UpdateItem = async (item) => {
+  const query = ` UPDATE Carteiras SET 
+                  FundoId = '${item.FundoId}',
+                  DtLancamento = '${item.DtLancamento}',
+                  Tipo = '${item.Tipo}',
+                  Qtd = '${item.Qtd}',
+                  Valor = '${item.Valor}'
+                  WHERE Id = '${item.Id}'`;
+  try {
+    await pool.connect();
+    await pool.request().query(query);
+    return true;
+  } catch (error) {
+    return false
+  }
+}
+
+export const RemoveItem = async (id) => {
+  const query = ` DELETE FROM Carteiras WHERE Id = '${id}'`;
+
+  try {
+    await pool.connect();
+    await pool.request().query(query);
+    return true;
+  } catch (error) {
+    return false
+  }
+}
