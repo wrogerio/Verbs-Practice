d38dff27caa14e8375914e375597f8e123b3fa2c
wellington <wrogeriowrs@gmail.com>
2023-05-02 19:41:02 -0300

    estrutura

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const Navbar = () => {
+  const router = useRouter();
+  const currentRoute = router.pathname;
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleMenu = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" href="/">
+          📖 Verbs Practice 📖
+        </Link>
+        <button onClick={() => toggleMenu()} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={expanded ? "collapse navbar-collapse show" : "collapse navbar-collapse"} id="navbarNavAltMarkup">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <Link className={currentRoute == "/" ? "nav-link active" : "nav-link"} href="/" onClick={() => toggleMenu()}>
+                <i className="fas fa-home me-1"></i> Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className={currentRoute == "/instituicoes" ? "nav-link active" : "nav-link"} href="/instituicoes" onClick={() => toggleMenu()}>
+                <i className="fas fa-book me-1"></i>
+                Verbs
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className={currentRoute == "/fundos" ? "nav-link active" : "nav-link"} href="/fundos" onClick={() => toggleMenu()}>
+                <i class="fas fa-head-side-brain me-1"></i>
+                Memory
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
